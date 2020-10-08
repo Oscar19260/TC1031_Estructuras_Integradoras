8362ee1e5cdc4484e611d9ae103eb5ee7edf9b00
Oscar19260 <61431500+Oscar19260@users.noreply.github.com>
2020-10-08 01:02:01 -0500

    Add files via upload

--- Actividad Integral 2.- Estructura de datos lineales (Evidencia Competencia)/main.cpp
+++ Actividad Integral 2.- Estructura de datos lineales (Evidencia Competencia)/main.cpp
@@ -1,78 +1,85 @@
-/*
- * main.cpp
- *
- *  Created on: 10/08/2020
- *      Author: bvaldesa
- */
-
 #include <iostream>
+#include <fstream>
+#include <list>
 #include <string>
-#include <sstream>
-#include <vector>
-#include <cstring>
-#include <string>
-#include "sorts.h"
+#include "list.h"
 
-using namespace std;
+using namespace std; 
 
+// obtenerContenido ////////////////////////////////////////
 template <class T>
-string arrayToString(const vector<T> &v) {
-	stringstream aux;
+bool obtenerContenido(List<T> &l1, string archivo){
+    ifstream read(archivo);
+    if(!read){
+        cout<<"Error no se pudo abrir el archivo: "<<archivo<<endl;
+        return false;
+    }
+    string dato;
+    while (getline(read, dato)){
+        if(dato.size() > 0){
+            l1.add(dato);
+		}
+    }
+    read.close();
+    return true;
+}
 
-	aux << "[" << v[0];
-	for (int i = 1; i < v.size(); i++) {
-		aux << ", " << v[i];
-	}
-	aux << "]";
-	return aux.str();
+int main(){
+    List<string> l1;
+    string dato; 
+    bool info = obtenerContenido(l1,"input.txt");
+    int opc;
+    do {
+        cout<<"\n--------Bienvenido--------\n";
+        cout<<"1.- Agregar Persona\n";
+        cout<<"2.- Mostrar lista\n";
+        cout<<"3.- Buscar Persona\n";
+        cout<<"4.- Remover Persona\n";
+        cout<<"5.- Limpiar lista\n";
+        cout<<"6.- Exit\n";
+        cin>>opc;
+        if(opc == 1){
+            cout<<"Introduce Matricula-Nombre: ";
+            cin>>dato;
+            l1.add(dato);
+            cout<<"\tNueva persona "<< dato << " fue agregada exitosamente\n";    
+        }
+        if(opc == 2){
+            l1.show();
+            cout<<"\n"; 
+        }
+
+        if(opc == 3){
+            cout<<"Introduce Matricula-Nombre: ";
+            cin>>dato;
+            cout<<l1.find(dato); 
+        }
+
+        if(opc == 4){
+            cout<<"Introduce Matricula-Nombre: ";
+            cin>>dato;
+            l1.remove(dato);
+        }
+
+        if(opc == 5){
+            l1.clear();
+        }
+
+        if(opc == 6){
+            l1.write("output.txt");
+            cout<<"Fin de la transmision"<<endl;
+        }
+    } while(opc != 6); 
+    return 0;
 }
 
 
-int main(int argc, char* argv[]) {
-
-	int array1[] = {58, 92, 75, 5, 21, 92, 84, 91, 64, 54, 3, 28, 11, 61, 65, 4, 1, 8, 52, 78};
-	vector<int> original1 (array1, array1 + sizeof(array1) / sizeof(int) );
-
-	int array2[] = {120, 10, 5, 897, 333, 68};
-	vector<int> original2 (array2, array2 + sizeof(array2) / sizeof(int) );
-
-	vector<int> prueba;
-	Sorts<int> sorts;
-	string resp1 = "[1, 3, 4, 5, 8, 11, 21, 28, 52, 54, 58, 61, 64, 65, 75, 78, 84, 91, 92, 92]";
-	string resp2 = "[5, 10, 68, 120, 333, 897]";
-
-	prueba = original1;
-	sorts.ordenaSeleccion(prueba);
-	cout << " 1 " <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original2;
-	sorts.ordenaSeleccion(prueba);
-	cout << " 2 " <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original1;
-	sorts.ordenaBurbuja(prueba);
-	cout << " 3 " <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original2;
-	sorts.ordenaBurbuja(prueba);
-	cout << " 4 " <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original1;
-	sorts.ordenaMerge(prueba);
-	cout << " 5 " <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original2;
-	sorts.ordenaMerge(prueba);
-	cout << " 6 " <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-
-	sorts.ordenaMerge(prueba);
-  cout << " 7 " <<	(5 == sorts.busqSecuencial(prueba, 897) ? "success\n" : "fail\n");
-
-	cout << " 8 " <<	(-1 == sorts.busqSecuencial(prueba, 100) ? "success\n" : "fail\n");
-
-	cout << " 9 " <<	(4 == sorts.busqBinaria(prueba, 333) ? "success\n" : "fail\n");
-
-	cout << " 10 " <<	(3 == sorts.busqBinaria(prueba, 197) ? "success\n" : "fail\n");
-
-}
+/*
+    ofstream write("example.txt");
+    if(write.is_open()){
+        for (string & dato: l1){
+        write<< dato <<endl;
+        }
+        write.close();
+    }
+*/
